da5ee620ef4b7ac6a5d5d29b7eda49a613024f3d
yuuka_miya <1886269+yuuka-miya@users.noreply.github.com>
2026-01-18 10:34:26 +0800

    Significantly rewrote the load analysis module to speed it up at least 10x

--- analysis/reader_timed_optimize.py
+++ analysis/reader_timed_optimize.py
@@ -1,10 +1,14 @@
+import cudf.pandas
+cudf.pandas.install()
+
 import pandas as pd
 import numpy as np
 import os
 import datetime
 import json
 import itertools
-import cudf
+
+import time
 
 from tqdm import tqdm
 #from multiprocessing import Pool
@@ -95,31 +99,19 @@ def pairwise(iterable):
     return list(a), list(b)
     
 def unpack_column(data_row):
-  global df_fin
-  global df_temp
-  global counter
+  #global df_fin
+  #global df_temp
+  #global counter
   
-  orig = replace_jointcode(data_row["ORIGIN_PT_CODE"])
-  dest = replace_jointcode(data_row["DESTINATION_PT_CODE"])
+  orig = data_row["ORIGIN_PT_CODE"]
+  dest = data_row["DESTINATION_PT_CODE"]
   #print(orig, dest)
   a, b = pairwise(data[orig][dest])
   a.pop()
-  pairs_len = len(b)
-  df_data = pd.DataFrame([data_row])
-  df_data = df_data.loc[df_data.index.repeat(pairs_len)]
+  df_data = data_row
   df_data["ORIGIN_PT_CODE"] = a
   df_data["DESTINATION_PT_CODE"] = b
-  if len(df_data.index) > 0:
-    df_temp = pd.concat([df_temp, df_data])
-    #make sure df_temp doesn't get too big
-    counter = counter + 1
-    if counter > 300:
-      #df_temp = df_temp.groupby(['DAY_TYPE', 'ORIGIN_PT_CODE', 'DESTINATION_PT_CODE', 'TIME_PER_HOUR']).sum()
-      #df_fin = df_fin.append(cudf.DataFrame.from_pandas(df_temp))
-      df_fin = cudf.concat([df_fin, cudf.DataFrame.from_pandas(df_temp)])
-      #df_fin = df_fin.append(df_temp)
-      df_temp = pd.DataFrame()
-      counter = 0
+  return df_data
 
 month = input("Dataset for (YYYYMM): ")
 weekdays = input("Number of weekdays: ")
@@ -151,26 +143,32 @@ with open('train_routes_nx.json') as json_file:
     
 tqdm.pandas()
 
-df_fin = cudf.DataFrame()
-df_temp = pd.DataFrame()
-df1.progress_apply(unpack_column, axis=1)
+df_fin = pd.DataFrame()
+
+
+start_gen = time.perf_counter()
+df_fin = df1.progress_apply(unpack_column, axis=1)
+start_concat = time.perf_counter()
+#chunk_out = pd.concat(chunk_out, ignore_index=True)
+df_fin = df_fin.explode(['ORIGIN_PT_CODE', 'DESTINATION_PT_CODE'])
+end_concat = time.perf_counter()
+#df1.progress_apply(unpack_column, axis=1)
 
 #pack up the stragglers
 #df_temp = df_temp.groupby(['DAY_TYPE', 'ORIGIN_PT_CODE', 'DESTINATION_PT_CODE', 'TIME_PER_HOUR']).sum()
-df_fin = df_fin = cudf.concat([df_fin, cudf.DataFrame.from_pandas(df_temp)])
 #df_fin = df_fin.append(df_temp)
-
-df_fin = df_fin.groupby(['DAY_TYPE', 'ORIGIN_PT_CODE', 'DESTINATION_PT_CODE', 'TIME_PER_HOUR']).sum()
-
-df_fin = df_fin.to_pandas()
-
-df_graphs = df_fin
 
 #copy out, for graph generation
 
-df_fin1 = pd.pivot_table(df_fin, index=['DAY_TYPE', 'ORIGIN_PT_CODE', 'DESTINATION_PT_CODE'], columns=["TIME_PER_HOUR"], aggfunc={'TOTAL_TRIPS': np.sum})
+df_fin1 = pd.pivot_table(df_fin, index=['DAY_TYPE', 'ORIGIN_PT_CODE', 'DESTINATION_PT_CODE'], columns=["TIME_PER_HOUR"], aggfunc={'TOTAL_TRIPS': "sum"})
 
-df_fin = df_fin.groupby(['DAY_TYPE', 'ORIGIN_PT_CODE', 'DESTINATION_PT_CODE']).sum()
+
+df_fin = df_fin.drop(columns=['PT_TYPE', 'YEAR_MONTH', 'TIME_PER_HOUR'])
+df_fin = df_fin.groupby(['DAY_TYPE', 'ORIGIN_PT_CODE', 'DESTINATION_PT_CODE']).agg("sum")
+
+end_groupby = time.perf_counter()
+
+print(f"GPU Concat {end_concat - start_concat}, GroupBy {end_groupby - end_concat}")
 
 df_fin.to_csv(os.path.join(os.getcwd(), "..", "processed_data", month, "cda_nx_opt_train_" + month + "_summary_" + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + ".csv"))
 df_fin1.to_csv(os.path.join(os.getcwd(), "..", "processed_data", month, "cda_nx_opt_train_" + month + "_byhour_" + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + ".csv"))
